20cb8730a575fdcdc654107f58972fce33385332
41415711 <41415711@090013-P33S13.macklabs.br>
2015-03-18 10:32:41 -0300

    Esboço do site

---
+++ Principal.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html >
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>Preço dos Combustívels</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<!--<link href="bootstrapcss.css" rel="stylesheet"> -->
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="estilo.css" rel="stylesheet">
+                <link href="script.js" rel=""
+	</head>
+	<body>
+<!-- begin template -->
+<div class="navbar navbar-custom navbar-fixed-top">
+ <div class="navbar-header"><a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Quem Somos</a></li>
+        <li><a href="#">Entenda o Preço da gasolina</a></li>
+        <li>&nbsp;</li>
+      </ul>
+      <form class="navbar-form">
+        <div class="form-group" style="display:inline;">
+          <div class="input-group">
+            <div class="input-group-btn">
+              
+            </div>
+            
+          </div>
+        </div>
+      </form>
+    </div>
+</div>
+
+<div id="map-canvas"></div>
+<div class="container-fluid" id="main">
+  <div class="row">
+  	<div class="col-xs-8" id="left">
+    
+            <h2>Preço dos Combustíveis</h2><br>
+            <input type="text" class="form-control" placeholder="Origem:">
+            <input type="text" class="form-control" placeholder="Destino:">
+            <input type="button" name="Pesquisar" value="Buscar"><br>
+            <!--<span class="input-group-addon"><span class="glyphicon glyphicon-search"></span> </span> -->
+      
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234166.00962261064!2d-47.05329175751311!3d-23.502131400124572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce448183a461d1%3A0x9ba94b08ff335bae!2sS%C3%A3o+Paulo+-+SP!5e0!3m2!1spt-BR!2sbr!4v1426529270141" width="600" height="450" frameborder="0" style="border:0"></iframe>
+      
+      <hr>
+      <!-- /item list -->
+      
+      <p>
+      <a href="http://www.bootply.com/render/129229"></a>  <a href="http://bootply.com/129229"></a>
+      </p>
+      
+      <hr> 
+        
+      <p>
+      <a href="http://bootply.com" target="_ext" class="center-block btn btn-primary"></a>
+      </p>
+        
+      <hr>      
+
+    </div>
+    <div class="col-xs-4"><!--map-canvas will be postioned here--></div>
+    
+  </div>
+</div>
+<!-- end template -->
+
+	<!-- script references -->
+		<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="js/bootstrapjs.js"></script>
+		<script src="http://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed"></script>
+		<script src="js/javascript.php"></script>
+  -->
+  
+	</body>
+</html>
+
+
+
+<?php
+
+?>
