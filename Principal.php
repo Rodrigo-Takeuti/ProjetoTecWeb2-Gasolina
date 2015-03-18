7e695474f93eaf38f091bb30ae9f0af2a9be3a0a
No Name <No Name@Windows-PC.Home>
2015-03-18 18:39:34 -0300

    Search box funcionando para localização dos postos se digitar postos de gasolina.

--- Principal.php
+++ Principal.php
@@ -11,9 +11,11 @@
 			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 		<link href="estilo.css" rel="stylesheet">
-                <link href="script.js" rel=""
-	</head>
-	<body>
+                <script type ="text/javascript" src="script.js"></script>
+                <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
+
+        </head>
+        <body onload="initialize()">
 <!-- begin template -->
 <div class="navbar navbar-custom navbar-fixed-top">
  <div class="navbar-header"><a class="navbar-brand" href="#">Logo</a>
@@ -43,39 +45,38 @@
     </div>
 </div>
 
-<div id="map-canvas"></div>
-<div class="container-fluid" id="main">
-  <div class="row">
-  	<div class="col-xs-8" id="left">
-    
-            <h2>Preço dos Combustíveis</h2><br>
-            <input type="text" class="form-control" placeholder="Origem:">
-            <input type="text" class="form-control" placeholder="Destino:">
-            <input type="button" name="Pesquisar" value="Buscar"><br>
-            <!--<span class="input-group-addon"><span class="glyphicon glyphicon-search"></span> </span> -->
+<div>
+    <div class="container-fluid" id="main">
+        <div class="row">
+            <div class="col-xs-8" id="left">
+                <br>
+                <h2>Preço dos Combustíveis</h2><br>
+                <input type="hidden" id="latitude"/>
+                <input type="hidden" id="longitude"/>
+                <input id="pac-input" class="controls" type="text" placeholder="Search Box"/>
+                <input type="button" value="Buscar" onclick="initialize()"/><br><br>
+                <div id="map" style="width: 600px; height: 450px"></div>
+                <!--<span class="input-group-addon"><span class="glyphicon glyphicon-search"></span> </span> -->
+            <hr>
+            <!-- /item list -->
       
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234166.00962261064!2d-47.05329175751311!3d-23.502131400124572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce448183a461d1%3A0x9ba94b08ff335bae!2sS%C3%A3o+Paulo+-+SP!5e0!3m2!1spt-BR!2sbr!4v1426529270141" width="600" height="450" frameborder="0" style="border:0"></iframe>
+             <p>
+                <a href="http://www.bootply.com/render/129229"></a>  <a href="http://bootply.com/129229"></a>
+            </p>
       
-      <hr>
-      <!-- /item list -->
-      
-      <p>
-      <a href="http://www.bootply.com/render/129229"></a>  <a href="http://bootply.com/129229"></a>
-      </p>
-      
-      <hr> 
+            <hr> 
         
-      <p>
-      <a href="http://bootply.com" target="_ext" class="center-block btn btn-primary"></a>
-      </p>
+            <p>
+                <a href="http://bootply.com" target="_ext" class="center-block btn btn-primary"></a>
+            </p>
         
-      <hr>      
+            <hr>
 
-    </div>
-    <div class="col-xs-4"><!--map-canvas will be postioned here--></div>
+            </div>
     
-  </div>
-</div>
+        </div>
+    </div>
+</div>    
 <!-- end template -->
 
 	<!-- script references -->
