2511f03d003b28ca42f3d82a0c6ec981e1568ffd
41415711 <41415711@090013-P33S13.macklabs.br>
2015-03-26 11:55:42 -0300

    Esboço da Página: Quem Somos

---
+++ QuemSomos.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html >
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>Preço dos Combustíveis</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<!--<link href="bootstrapcss.css" rel="stylesheet"> -->
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="estilo.css" rel="stylesheet">
+                <script type ="text/javascript" src="script.js"></script>
+                <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
+
+        </head>
+        <body onload="initialize()">
+<!-- begin template -->
+<div class="navbar navbar-custom navbar-fixed-top">
+ <div class="navbar-header"><a class="navbar-brand" href="#">Logo</a>
+      <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Quem Somos</a></li>
+        <li><a href="#">Entenda o Preço da gasolina</a></li>
+        <li>&nbsp;</li>
+      </ul>
+      <form class="navbar-form">
+        <div class="form-group" style="display:inline;">
+          <div class="input-group">
+            <div class="input-group-btn">
+              
+            </div>
+            
+          </div>
+        </div>
+      </form>
+    </div>
+</div>
+
+<div>
+    <div class="container-fluid" id="main">
+        <div class="row">
+            <div class="col-xs-8" id="left">
+                <br>
+                <h2>Quem Somos?</h2><br>
+                
+                
+                <p class="P1">Somos uma equipe de estudantes de Ciência da Computação e desenvolvemos esse site com o objetivo de ajudar os usuários motoristas a encontrar os postos de gasolina com os melhores preços dos combustíveis. Levando em conta o atual preço da gasolina, achamos que esse site seria uma boa solução apara aqueles que querem encotrar os melhores postos de gasolina para abastecer seu carro. Aqui você fará a busca pelos postos da região desejada e encotrará todos os postos presentes e os preços dos combustíveis(Gasolina, Álcool e Diesel). Esperamos que nosso site seja de extrema utilidade para todos vocês usuários!</p>
+                <p class="P2"> Agradecemos desde já o seu apoio - Amanda, Leonardo e Rodrigo. </p>                    
+                
+            <!-- /item list -->
+      
+            
+
+            </div>
+    
+        </div>
+    </div>
+</div>    
+<!-- end template -->
+
+	<!-- script references -->
+		<!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+		<script src="js/bootstrapjs.js"></script>
+		<script src="http://maps.googleapis.com/maps/api/js?sensor=false&extension=.js&output=embed"></script>
+		<script src="js/javascript.php"></script>
+  -->
+  
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+?>
