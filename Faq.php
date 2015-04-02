fd5d61e4e49af9e93371250248fb98e426b8455a
41404671 <41404671@089978-P33S13.macklabs.br>
2015-04-02 12:01:34 -0300

    pagina add

---
+++ Faq.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html >
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>FAQ</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="estilo.css" rel="stylesheet">
+                <script type ="text/javascript" src="script.js"></script>
+                <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places"></script>
+
+        </head>
+        <body onload="initialize()">
+<!-- begin template -->
+<div class="navbar navbar-custom navbar-fixed-top">
+    <div class="navbar-header"><a class="navbar-brand" href="Principal.php">Logo</a>
+      <a class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+         <li class="active"><a href="Principal.php">Home</a></li>
+        <li><a href="QuemSomos.php">Quem Somos</a></li>
+        <li><a href="GasolinaCara.php">Entenda o Preço da gasolina</a></li>
+        <li><a href="Faq.php">FAQ</a></li>
+        <li>&nbsp;</li>
+      </ul>
+      <form class="navbar-form">
+        <div class="form-group" style="display:inline;">
+          <div class="input-group">
+            <div class="input-group-btn">
+              
+            </div>
+            
+          </div>
+        </div>
+      </form>
+    </div>
+</div>
+
+<div>
+    <div class="container-fluid" id="main">
+        <div class="row">
+            <div class="col-xs-8" id="left">
+                <br>
+                <h2>FAQ - Perguntas frequentes</h2><br>
+                
+ <h3>Os preços são atuais? Qual a periodicidade de atualização?</h3>
+Sim, os preços são atualizados semanalmente. O sistema utiliza dados coletados pela Agência Nacional do Petróleo, 
+Gás Natural e Biocombustíveis (ANP). A ANP coleta os dados durante a semana. 
+No final de semana, nosso sistema obtém os dados, associa com a localização geográfica dos postos e 
+imediatamente disponibiliza através do website.
+<hr></hr>
+
+<h3>Não aparecem postos na minha cidade. Qual o motivo?</h3>
+A Agência Nacional do Petróleo, Gás Natural e Biocombustíveis (ANP) coleta apenas uma amostra dos preços dos postos do Brasil,
+focando principalmente nas cidades mais populosas e regiões distintas. Além disso, nosso sistema remove postos com dados desatualizados. 
+Isso restringe o número de postos, porém garante a veracidade da informação apresentada.
+<hr></hr>
+
+<h3>Porque a navegação por cidade exibe mais postos do que o mapa?</h3>
+No mapa exibimos apenas os postos os quais temos a localização geográfica precisa. 
+Por hora, cerca de 20% dos postos não são apresentados no mapa devido a esta falta de precisão.
+Em breve, estaremos trabalhando para exibir todos os postos disponíveis no mapa interativo.
+<hr></hr>
+
+<h3>Como faço para ordenar por preço do álcool ou diesel?</h3>
+Por padrão, a lista de postos aparece ordenada pelo preço da gasolina, do mais barato ao mais caro.
+Para ordenar por álcool, diesel ou pelo nome do posto, basta clicar no cabeçalho da tabela.
+<hr></hr>
+
+<h3>Tenho uma idéia, como entro em contato? Como posso colaborar?</h3>
+No momento, estamos obtendo idéias e sugestões através do nosso fórum. 
+Entre e participe. Se preferir, poderá nos contatar através do e-mail de contato,
+através do Twitter ou comunidade no Orkut.
+<hr></hr>
+                <p class="P2"> Agradecemos desde já o seu apoio - Amanda, Leonardo e Rodrigo. </p>                    
+                
+            <!-- /item list -->
+      
+            
+
+            </div>
+    
+        </div>
+    </div>
+</div>    
+<!-- end template -->
+  
+  <br></br>
+  
+    <footer>
+      <p>Copyright</p>
+  </footer>
+  
+	</body>
+</html>
+
+
+
+
