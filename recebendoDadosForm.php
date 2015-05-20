c2e6a58b798923e1f283c4fafb65700518aef5a7
Rodrigo Takeuti <rodrigotakeuti@hotmail.com>
2015-05-20 08:13:29 -0300

    Tentando inserir dados do formulario para o banco de dados

--- recebendoDadosForm.php
+++ recebendoDadosForm.php
@@ -1,12 +1,13 @@
 <?php
-$nome = $_POST["nome"];
-$email = $_POST["email"];
-$nomePosto = $_POST["nomePosto"];
-$gasolina = $_POST["gasolina"];
-$alcool = $_POST["alcool"];
-$diesel = $_POST["diesel"];
-$erro = 0;
-
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    $nomePosto = $_POST["nomePosto"];
+    $gasolina = $_POST["gasolina"];
+    $alcool = $_POST["alcool"];
+    $diesel = $_POST["diesel"];
+    $erro = 0;
+    
     if(empty($nome)){
         echo 'Por favor, digite seu nome.<br>';
         $erro=1;
@@ -15,6 +16,11 @@ $erro = 0;
             echo 'Por favor, digite seu email corretamente.<br>'; 
             $erro=1;
         }
+            if(empty($senha))
+            {
+                echo 'Por favor, digite sua senha.<br>';
+                $erro=1;
+            }
             if(empty($nomePosto)){
                 echo 'Por favor, informe o nome do seu posto.';
                 $erro=1;
@@ -32,10 +38,12 @@ $erro = 0;
                     $erro=1;
                 }
                     if($erro==0){
+                        
+                        
                        echo'Cadastro efetuado com sucesso!<br>';
+                       include "insere.php";
                     }else{
                         echo'Erro no cadastro.'; 
                     }
-
 ?>
 
