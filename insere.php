8313772019899b77393a533d2aa96e012d67f80e
Rodrigo Takeuti <rodrigotakeuti@hotmail.com>
2015-05-20 08:50:08 -0300

    Atualização, não funcional ainda

--- insere.php
+++ insere.php
@@ -5,12 +5,13 @@ include "conecta_mysql.php";
     $email = $_POST["email"];
     $senha = $_POST["senha"];
     $nomePosto = $_POST["nomePosto"];
+    $endereco = $_POST["endereco"];
     $gasolina = $_POST["gasolina"];
     $alcool = $_POST["alcool"];
     $diesel = $_POST["diesel"];
     
     $sql = "INSERT INTO postosgasolina VALUES";
-    $sql .= "('$nome' , '$email' , '$senha' , '$nomePosto' , '$gasolina' , '$diesel' , '$alcool')";
+    $sql .= "('$nome' , '$email' , '$senha' , '$nomePosto' , '$endereco' , '$gasolina' , '$alcool' , '$diesel')";
     $resultado = mysqli_query($conexao, $sql);
     
     mysqli_close($conexao);
